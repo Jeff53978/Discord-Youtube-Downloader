36abaaae0ef38de52ca487dff83c6e867ed93f5f
Snikker <snikker.zakelijk@protonmail.com>
2022-02-13 23:21:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from concurrent.futures import thread
-import discord, os, pyautogui, requests, cv2, numpy, subprocess, threading
+import discord, os, pytube
 from config import *
 
 client = discord.Bot(intents=discord.Intents.all())
@@ -8,10 +7,16 @@ client = discord.Bot(intents=discord.Intents.all())
 async def on_ready():
     print(f" > Logged in as: {client.user.name}#{client.user.discriminator}")
 
-@client.slash_command(name="download", guild_ids=guildid)
-async def killall(ctx, url):
-    embed = discord.Embed(title="Downloading Video", description=f"```Url:\n{url}```", color=0x660cf)
+@client.slash_command(name="download")
+async def download(ctx, link):
+    embed = discord.Embed(title="Downloading Video", description=f"```{link}```", color=0x660cf)
     await ctx.respond(embed=embed)
-    
+    url = pytube.YouTube(str(link))
+    video = url.streams.get_highest_resolution()
+    video.download(os.getenv('TEMP'), filename="video.mp4")
+    try:
+        await ctx.respond(file=discord.File(f"{os.getenv('TEMP')}\\video.mp4"))
+    except Exception as error:
+        embed = discord.Embed(title="Error sending video", description=f"```{error}```", color=0x660cf)
 
 client.run(token)
